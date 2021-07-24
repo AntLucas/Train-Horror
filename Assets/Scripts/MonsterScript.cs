82008f9c4e9e0b0186fe5b4cfae1ea298f22ce49
Antlucas <antonio.lucas278@outlook.com>
2021-07-24 20:21:05 -0300

    Adiciona modo de se esconder atrás das malas, perseguição do monstro e animação de ataque

--- Assets/Scripts/MonsterScript.cs
+++ Assets/Scripts/MonsterScript.cs
@@ -6,16 +6,20 @@ public class MonsterScript : MonoBehaviour
 {
     public float speed;
 
+    private Animator anim;
     Rigidbody2D rb;
     bool facingRight = false;
     bool noChao = false;
     Transform groundCheck;
+
+    public GameObject jogador;
 
     
     // Start is called before the first frame update
     void Start()
     {
      rb = gameObject.GetComponent<Rigidbody2D>();
+     anim = gameObject.GetComponent<Animator>();
      groundCheck = transform.Find("MonsterGroundCheck");   
     }
 
@@ -27,16 +31,50 @@ public class MonsterScript : MonoBehaviour
         if(!noChao) {
             speed *= -1;
         }
+
+        if ((jogador.transform.position.x + 1.2 < transform.position.x || jogador.transform.position.x - 1.2 > transform.position.x) || (jogador.transform.position.x > -1 && jogador.transform.position.x < 1) || (jogador.transform.position.x > -4 && jogador.transform.position.x < -2)) {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        } else if (jogador.transform.position.x + 1.2 >= transform.position.x || jogador.transform.position.x - 1.2 <= transform.position.x){
+            anim.SetTrigger("Atacou");
+        }  
+            if ((jogador.transform.position.x >= -1 && jogador.transform.position.x <= 1) || (jogador.transform.position.x >= -4 && jogador.transform.position.x <= -2)) {
+                rb.velocity = new Vector2(speed, rb.velocity.y);
+                
+                if( speed > 0) {
+                    speed = 1;
+                } else {
+                    speed = -1;
+                }
+                
+            } else {
+                
+                if (jogador.transform.position.x < transform.position.x && speed > 0)  {
+                    speed = -2;
+                } else if (jogador.transform.position.x > transform.position.x && speed < 0) {
+                    speed = 2;
+                } else if (jogador.transform.position.x < transform.position.x && speed < 0) {
+                    speed = -2;
+                } else if (jogador.transform.position.x > transform.position.x && speed > 0) {
+                    speed = 2;
+                }
+            }
+
+
+
     }
 
     void FixedUpdate() {
-        rb.velocity = new Vector2(speed, rb.velocity.y);
+        
+        
+    
 
         if (speed > 0 && !facingRight) {
             Flip();
         } else if (speed < 0 && facingRight) {
             Flip();
         }
+
+
     }
 
     void Flip() 
@@ -50,16 +88,25 @@ public class MonsterScript : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.CompareTag("Player")) {
-            speed *= -1;
+            anim.SetTrigger("Atacou");
         }
     }
 
-    
     void OnCollisionEnter2D(Collision2D other)
     {
         if(other.gameObject.CompareTag("Player"))
-        {
-            other.gameObject.GetComponent<PlayerLife>().perdeVida();
+        {   
+            if ((jogador.transform.position.x >= -1 && jogador.transform.position.x <= 1) || (jogador.transform.position.x >= -4 && jogador.transform.position.x <= -2)) {
+               
+                
+                
+                
+            } else {
+                
+                other.gameObject.GetComponent<PlayerLife>().perdeVida();
+            }
+            
         }
     }
+
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float speed;
 
     private Rigidbody2D rb;
+
     private bool facingRight = true;
     private Animator anim;
 
@@ -20,16 +21,39 @@ public class PlayerController : MonoBehaviour
         rb = gameObject.GetComponent<Rigidbody2D>();
         anim = gameObject.GetComponent<Animator>();
         groundCheck = gameObject.transform.Find("GroundCheck");
+        
+           
     }
 
     // Update is called once per frame
     void Update()
     {
         noChao = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+
+    //     if (Input.GetKeyDown(KeyCode.X)) {
+    //     Vector3 theScale = transform.localScale;
+    //     float a = 4.5f;
+    //     theScale.x += a;
+    //     theScale.y += a;
+    //     transform.localScale = theScale;
+    // }
+
+    // if (Input.GetKeyDown(KeyCode.B)) {
+        
+    //      Vector3 theScale = transform.localScale;
+    //     theScale.x *= 0;
+    //     theScale.y *= 0;
+    //     transform.localScale = theScale;
+    // }
+
+        
+       
     }
 
     void FixedUpdate()
     {
+       
         float h = Input.GetAxisRaw("Horizontal");
         anim.SetFloat("Velocidade", Mathf.Abs(h));
 
@@ -40,15 +64,50 @@ public class PlayerController : MonoBehaviour
         } else if (h < 0 && facingRight) {
             Flip();
         }
+
+        if ((transform.position.x > -1 && transform.position.x < 1) || transform.position.x > -4 && transform.position.x < -2) {
+            Esconder();
+        } else {
+            Aparecer();
+        }
+
     }
 
     void Flip() 
     {
         facingRight = !facingRight;
-
         Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
+        if(theScale.x == 0f && facingRight == true) {
+            theScale.x = 4.5f;
+        } else if (theScale.x == 0f && facingRight == false){
+            theScale.x = -4.5f;
+        } else {
+            theScale.x *= -1;
+        }
+        
         transform.localScale = theScale;
     }
 
+    void Esconder() {
+    
+        Vector3 theScale = transform.localScale;
+        theScale.x *= 0;
+        theScale.y *= 0;
+        transform.localScale = theScale;   
+        
+    }
+
+    void Aparecer() {
+        Vector3 theScale = transform.localScale;  
+        float h = Input.GetAxisRaw("Horizontal");
+        if(h > 0.0) {
+            facingRight = true;
+            theScale.x = 4.5f; 
+        } else if (h < 0.0) {
+            facingRight = false;
+            theScale.x = -4.5f;
+        }
+        theScale.y = 4.5f;
+        transform.localScale = theScale;
+    }
 }
