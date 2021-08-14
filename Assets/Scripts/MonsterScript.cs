f71926430abf9b960accb95d901719e8fbda8f0d
Antlucas <antonio.lucas278@outlook.com>
2021-08-14 17:02:10 -0300

    Adiciona método de subir no vagão ou ficar entre vagões

--- Assets/Scripts/MonsterScript.cs
+++ Assets/Scripts/MonsterScript.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class MonsterScript : MonoBehaviour
 {
+
+    List<int> esconderijos = new List<int>();
+
     public float speed;
 
     private Animator anim;
@@ -27,7 +31,8 @@ public class MonsterScript : MonoBehaviour
      rb = gameObject.GetComponent<Rigidbody2D>();
      anim = gameObject.GetComponent<Animator>();
      groundCheck = transform.Find("MonsterGroundCheck");   
-     
+     esconderijos.Add(0);
+     esconderijos.Add(-3);
     }
 
 
@@ -43,24 +48,31 @@ public class MonsterScript : MonoBehaviour
         }
 
         if(jogador.gameObject.GetComponent<PlayerLife>().vivo) {
+            bool playerEncontrado = true;
+
+            foreach (int malas in esconderijos)
+            {
+
+                    if (jogador.transform.position.x > (malas -1) && jogador.transform.position.x < (malas + 1)) {
+
+                        playerEncontrado = false;
+
+                    }
+
+            }
             
-            if ((jogador.transform.position.x + 1.2 < transform.position.x || jogador.transform.position.x - 1.2 > transform.position.x) || (jogador.transform.position.x > -1 && jogador.transform.position.x < 1) || (jogador.transform.position.x > -4 && jogador.transform.position.x < -2)) {
-                rb.velocity = new Vector2(speed, rb.velocity.y);
-            } else if (jogador.transform.position.x + 1.2 >= transform.position.x || jogador.transform.position.x - 1.2 <= transform.position.x){
-                anim.SetTrigger("Atacou");
-                speed *= -1;
-            }  
-                if ((jogador.transform.position.x >= -1 && jogador.transform.position.x <= 1) || (jogador.transform.position.x >= -4 && jogador.transform.position.x <= -2)) {
+                if (!playerEncontrado || !jogador.gameObject.GetComponent<PlayerController>().embaixo || !jogador.gameObject.GetComponent<PlayerController>().embaixo || jogador.gameObject.GetComponent<PlayerController>().entreVagao) {
+                         
                     rb.velocity = new Vector2(speed, rb.velocity.y);
-                    
+                        
                     if( speed > 0) {
                         speed = 1;
                     } else {
                         speed = -1;
                     }
-                    
+                        
                 } else {
-                    
+                            
                     if (jogador.transform.position.x < transform.position.x && speed > 0)  {
                         speed = -2;
                     } else if (jogador.transform.position.x > transform.position.x && speed < 0) {
@@ -70,31 +82,32 @@ public class MonsterScript : MonoBehaviour
                     } else if (jogador.transform.position.x > transform.position.x && speed > 0) {
                         speed = 2;
                     }
+                        rb.velocity = new Vector2(speed, rb.velocity.y);
                 }
-
-            } else if (!playerVivo) {
+                    
+        }
+            
+        else if (!playerVivo) {
                 
 
-                if(jogador.transform.position.y > -1.2f) {
-                    if (this.fixedDeltaTime < 1) {
+            if(jogador.transform.position.y > -1.2f) {
+                if (this.fixedDeltaTime < 1) {
                         
-                        novaPosicao.y += -0.002f;
-                        novaPosicao.x = jogador.transform.position.x;
-                        jogador.transform.position = novaPosicao;
+                    novaPosicao.y += -0.0035f;
+                    novaPosicao.x = jogador.transform.position.x;
+                    jogador.transform.position = novaPosicao;
                         
-                    }
-                } else {
-                        novaPosicao.y = -1.2f;
-                        novaPosicao.x = jogador.transform.position.x;
-                        jogador.transform.position = novaPosicao;
                 }
-
-                
-                anim.SetTrigger("Atacou");
-                   
+            } else {
+                     novaPosicao.y = -1.2f;
+                     novaPosicao.x = jogador.transform.position.x;
+                     jogador.transform.position = novaPosicao;
             }
+   
+            anim.SetTrigger("Atacou");
+                   
+        }
         
-
     }
 
     void FixedUpdate() {
@@ -114,16 +127,6 @@ public class MonsterScript : MonoBehaviour
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;
         transform.localScale = theScale;
-    }
-
-    void OnTriggerEnter2D(Collider2D other) {
-
-        if (other.gameObject.CompareTag("Player")) {
-            anim.SetTrigger("Atacou");
-            speed *= -1;
-        } else if (other.gameObject.CompareTag("Monster")){
-            speed *= -1;
-        }
     }
 
     void OnCollisionEnter2D(Collision2D other)
